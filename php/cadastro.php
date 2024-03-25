b36f4c732ab3877cfca30aa96e67944216bd58be
Samuel Alves <samuelalvess006@gmail.com>
2024-03-24 23:34:32 -0300

    primeiro commit

---
+++ php/cadastro.php
@@ -0,0 +1,20 @@
+<?php
+    include("conexao.php");
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $sql = "INSERT INTO usuario(nome, email, senha) 
+            VALUES ('$nome', '$email', '$senha')";
+
+    if (mysqli_query($conexao, $sql)) {
+        echo "Usuário Cadastrado com sucesso";
+    }
+
+    else {
+        echo "Erro:".mysqli_connect_error($conexao);
+    }
+
+    mysqli_close($conexao);
+?>
